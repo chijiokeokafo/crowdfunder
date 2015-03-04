3d03bf8186b125bb9e702239abfde2b9f6962f79
Chijioke Okafo <chijiokeokafo@gmail.com>
2015-03-04 14:44:34 -0500

    Added owner_id Column to project table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150303194025) do
+ActiveRecord::Schema.define(version: 20150304193409) do
 
   create_table "pledges", force: :cascade do |t|
     t.integer  "amount"
@@ -30,7 +30,10 @@ ActiveRecord::Schema.define(version: 20150303194025) do
     t.string   "category"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.integer  "owner_id"
   end
+
+  add_index "projects", ["owner_id"], name: "index_projects_on_owner_id"
 
   create_table "rewards", force: :cascade do |t|
     t.string   "description"
